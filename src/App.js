cdc8752f62794b0e82c146536fa54d6997f958f4
hellomanishahere <manishajain5814@gmail.com>
2026-05-03 12:24:18 +0530

    add basic localStorage persistence and display expense list

--- src/App.js
+++ src/App.js
@@ -1,18 +1,37 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import ExpenseForm from "./components/ExpenseForm";
 
 function App() {
   const [expenses, setExpenses] = useState([]);
 
+  // this'll load from localStorage
+  useEffect(() => {
+    const stored = JSON.parse(localStorage.getItem("expenses")) || [];
+    setExpenses(stored);
+  }, []);
+
+  //this is saving to localStorage
+  useEffect(() => {
+    localStorage.setItem("expenses", JSON.stringify(expenses));
+  }, [expenses]);
+
   const addExpense = (expense) => {
     setExpenses([...expenses, expense]);
-    console.log(expense);
   };
 
   return (
     <div>
       <h1>SpendWise</h1>
       <ExpenseForm onAddExpense={addExpense} />
+
+      <h2>All Expenses</h2>
+      <ul>
+        {expenses.map((e) => (
+          <li key={e.id}>
+            ₹{e.amount} - {e.category} - {e.date}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
